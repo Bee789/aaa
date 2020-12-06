f2a2bd2276b1c464b50574b6666f022dfd3550c8
Bee789 <1621523972@qq.com>
2020-12-06 19:50:33 +0800

    修改Dao

--- ssm_dao/src/test/java/com/itheima/dao/DaoTest.java
+++ ssm_dao/src/test/java/com/itheima/dao/DaoTest.java
@@ -12,7 +12,7 @@ public class DaoTest {
             Items items = new Items();
             items.setName("商品名称");
             items.setPrice(16666f);
-            items.setPrice(10f);
+            items.setPrice(100f);
             itemsDao.save(items);
         System.out.println("商品列表：：："+itemsDao.findAll());
         }
